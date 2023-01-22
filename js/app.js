fe726211c2c0937cf510d6b9e7147f899b643bc2
josephB200 <josephbenaroche@gmail.com>
2023-01-22 21:33:10 +0100

    test Js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-// const menuInscriptionConexion = document.querySelector('.account-responsive');
-// const showModal = document.querySelector('.modalInscriptionConexion')
-// let isModalOpen = false;
+const menuInscriptionConexion = document.querySelector('.account-responsive');
+const showModal = document.querySelector('.modalInscriptionConexion')
+let isModalOpen = false;
 
-// function ShowModalInscriptionConexion (event){
-//     event.preventDefault();
-//     showModal.style.color = "blue";
-// }
+function ShowModalInscriptionConexion (event){
+    event.preventDefault();
+    showModal.style.color = "blue";
+}
 
-// menuInscriptionConexion.addEventListener("click", function ShowModalInscriptionConexion() {
-//     if (isModalOpen) {
-//     showModal.style.display = "none";
-//     isModalOpen = false;
-//     } else {
-//     showModal.style.display = "flex" ;
-//     isModalOpen = true;
-//     }
-//     });
+menuInscriptionConexion.addEventListener("click", function ShowModalInscriptionConexion() {
+    if (isModalOpen) {
+    showModal.style.display = "none";
+    isModalOpen = false;
+    } else {
+    showModal.style.display = "flex" ;
+    isModalOpen = true;
+    }
+    });
